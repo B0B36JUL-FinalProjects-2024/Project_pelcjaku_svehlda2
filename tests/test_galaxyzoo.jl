4f5076c6e6bb3102f7e333a6750dc4dfe92fbc3d
Jakub Pelc <jakub.pelc@seznam.cz>
2025-02-05 18:24:26 +0100

    fix test galaxyzoo

--- tests/test_galaxyzoo.jl
+++ tests/test_galaxyzoo.jl
@@ -5,7 +5,7 @@ using .GalaxyZoo
 
 @testset "GalaxyZoo Module" begin
 	@testset "Image Preprocessing" begin
-		img_path = "dataset/images_training_rev1/100023.jpg"
+		img_path = "tests/test.jpg"
 		img_array = GalaxyZoo.preprocess_image(img_path)
 		@test size(img_array) == (52, 52, 3)  # check if the image is resized correctly
 		@test eltype(img_array) == Float32      # check if the image is converted to Float32
